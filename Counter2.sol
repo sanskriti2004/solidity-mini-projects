762f477ff73c78e99f447646aed625bded2849e5
sanskriti2004 <sanskritigogoi2004@gmail.com>
2025-02-27 18:39:42 +0530

    better counter

---
+++ Counter2.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+/**
+ * @title Counter
+ * @dev Simple contract to manage a counter with increment functionality
+ */
+contract Counter2 {
+
+    // here by making state public and setting default value we need not use a constructor hence saving gas
+    uint256 public count = 0; // State variable to store the counter value
+
+
+    /**
+     * @dev Increments the count by 1.
+     * @notice This function modifies the state, so it costs gas.
+     */
+    function incrementCount() public {
+        count += 1;
+    }
+}
