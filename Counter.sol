657d04f8674f11086d136e6eed742bd43bb5db2c
sanskriti2004 <sanskritigogoi2004@gmail.com>
2025-02-27 18:25:23 +0530

    simple counter

---
+++ Counter.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+/**
+ * @title Counter
+ * @dev Simple contract to manage a counter with increment functionality
+ */
+contract Counter {
+    uint256 private count; // State variable to store the counter value
+
+    /**
+     * @dev Retrieves the current count value.
+     * @return The current count value.
+     * @notice This function is `view`, meaning it does not modify the blockchain state and is free of gas costs.
+     */
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
+    /**
+     * @dev Increments the count by 1.
+     * @notice This function modifies the state, so it costs gas.
+     */
+    function incrementCount() public {
+        count += 1;
+    }
+}
