98890ce00d416a80aa6b43de5fdc28552296e2ce
sanskriti2004 <sanskritigogoi2004@gmail.com>
2025-02-27 18:39:19 +0530

    simple counter

--- Counter.sol
+++ Counter.sol
@@ -8,7 +8,12 @@ pragma solidity ^0.8.26;
 contract Counter {
     uint256 private count; // State variable to store the counter value
 
-    /**
+    // constructor is called when the contract is deployed and count is set to 0
+     constructor() public {
+        count = 0;
+     }
+
+     /**
      * @dev Retrieves the current count value.
      * @return The current count value.
      * @notice This function is `view`, meaning it does not modify the blockchain state and is free of gas costs.
